c79947b06c0be2e9c4f2517e7cc8895bf14e943a
Hyttalo Costa <147499168+EuHttl@users.noreply.github.com>
2024-09-20 17:17:27 +0000

    chore: changes links

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let header = document.getElementById('header')
+// const ctaproject = document.createElement("cta-project-site")
 
 window.addEventListener('scroll', () => {
     if(window.scrollY >= 200){
@@ -7,3 +8,21 @@ window.addEventListener('scroll', () => {
         header.style.background = 'transparent'
     }
 })
+
+// if(ctaproject == ''){
+//     Toastify({
+//         text: "Ops, O site está indisponivel no momento",
+//         duration: 3000,
+//         close: true,
+//         gravity: "top", // `top` or `bottom`
+//         position: "center", // `left`, `center` or `right`
+//         stopOnFocus: true, // Prevents dismissing of toast on hover
+//         style: {
+//           background: "linear-gradient(to right, #0000CD, #0000FF)",
+//         },
+//     }).showToast();
+
+//     return;
+
+// }
+
