14eee96fa2f11aa347c40998edcd803b5eda9346
Hyttalo Costa <147499168+EuHttl@users.noreply.github.com>
2025-01-08 00:41:30 +0000

    chore: specifical lines

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let header = document.getElementById('header')
-// const ctaproject = document.createElement("cta-project-site")
+const ctaproject = document.createElement("cta-project-site")
 
 window.addEventListener('scroll', () => {
     if(window.scrollY >= 200){
@@ -9,20 +9,20 @@ window.addEventListener('scroll', () => {
     }
 })
 
-// if(ctaproject == ''){
-//     Toastify({
-//         text: "Ops, O site está indisponivel no momento",
-//         duration: 3000,
-//         close: true,
-//         gravity: "top", // `top` or `bottom`
-//         position: "center", // `left`, `center` or `right`
-//         stopOnFocus: true, // Prevents dismissing of toast on hover
-//         style: {
-//           background: "linear-gradient(to right, #0000CD, #0000FF)",
-//         },
-//     }).showToast();
+if(ctaproject == ''){
+    Toastify({
+        text: "Ops, O site está indisponivel no momento",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #0000CD, #0000FF)",
+        },
+    }).showToast();
 
-//     return;
+return;
 
-// }
+ }
 
